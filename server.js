287f1ccc4f43c963b1a38a01008e7cd4be35db32
omril1 <omri2593@gmail.com>
2018-12-21 22:40:17 +0200

    Moved serverTiming

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/serverTiming', require('./serverTiming'));
+app.get('/serverTiming', require('./serverRoutes/serverTiming'));
 
 app.use(express.static('public'));
 
