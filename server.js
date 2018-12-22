47300d711ee2887ec1a349a33b549ddae4fbf816
omril1 <omri2593@gmail.com>
2018-12-22 14:33:16 +0200

    Added Set-Cookie header to every request

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((_req, res, next) => {
+  res.header('Set-Cookie', 'my_cookie=HelloWorld; Path=/; Expires=Wed, 15 Mar 2017 15:59:59 GMT');
+  next();
+});
 app.get('/serverTiming', require('./serverRoutes/serverTiming'));
 app.get('/restApi', require('./serverRoutes/restApi'));
 
